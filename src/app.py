6c579144439d296a4930c7f1d93b57ab1c9910d2
OlgaKurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-05-16 17:34:27 +0000

    streamlit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,90 @@
-from utils import db_connect
-engine = db_connect()
+
 
 # your code here
+import streamlit as st
+import pickle
+import pandas as pd
+
+# model
+with open('../models/LinearRegression_.sav', 'rb') as file:
+    model = pickle.load(file)
+
+# scaler
+with open('../models/scalerOM.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
+
+feature_names = scaler.get_feature_names_out()
+
+st.markdown('<style>h1 { color: brown; }</style>', unsafe_allow_html=True)
+# Website title
+st.title("Insurance Calculator")
+
+# mapping for region, sex and smoker
+region_mapping = {
+    'southwest' : 0, 
+    'southeast': 1, 
+    'northwest': 2, 
+    'northeast': 3
+    }
+
+smoker_mapping = {
+    'Yes': 0, 
+    'No': 1
+    }
+
+sex_mapping = {
+    'Female': 0, 
+    'Male' : 1
+    }
+
+# user input
+age = st.slider(
+    'Plase, enter the age:',
+    min_value=18,
+    max_value=70,
+    step = 1
+    )
+
+sex_n = st.selectbox(
+    'Plase, enter the sex:',
+    ['Female', 'Male']
+    )
+
+bmi = st.slider(
+    'Body mass index (BMI):',
+    min_value=15.0,
+    max_value=53.0, 
+    step = 0.1
+    )
+
+children = st.slider(
+    'Number of children:', 
+    min_value=0,
+    max_value=7, 
+    step = 1
+    )
+
+smoker_n = st.selectbox(
+    'Are you a smoker?', 
+    ['Yes', 'No'] 
+    )
+
+region_n = st.selectbox(
+    'Region:',
+    ['southwest', 'southeast', 'northwest', 'northeast'],
+    )
+
+if st.button("Predict"):
+    row = [
+        age,
+        sex_mapping[sex_n],
+        smoker_mapping[smoker_n],
+        children,
+        bmi,
+        region_mapping[region_n]
+        ]
+
+    scal_data = scaler.transform([row])
+    y_pred = model.predict([row])
+
+    st.write(f'The charge for your insurance is: ${y_pred[0]:,.2f}')
